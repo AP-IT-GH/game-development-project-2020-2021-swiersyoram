a8732ac37df631862a557b1f2a2666f8cc2d0640
yoram <yoram@swiers.be>
2020-11-09 00:19:41 +0100

    started designing spritesheet

--- 10op20/10op20/Game1.cs
+++ 10op20/10op20/Game1.cs
@@ -8,6 +8,9 @@ namespace _10op20
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D texture;
+        private Rectangle deelRectangle;
+        private int rectangleX = 0;
 
         public Game1()
         {
@@ -19,6 +22,7 @@ namespace _10op20
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            deelRectangle = new Rectangle(rectangleX, 0, 128,136);
 
             base.Initialize();
         }
@@ -26,7 +30,7 @@ namespace _10op20
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            texture = Content.Load<Texture2D>("ironman");
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,7 +46,18 @@ namespace _10op20
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Gray);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(texture, new Vector2(10, 10), deelRectangle, Color.White);
+            _spriteBatch.End();
+
+            rectangleX += 128;
+            
+            if (rectangleX > 512 )
+            {
+                rectangleX = 0;
+            }
+            deelRectangle.X = rectangleX;
 
             // TODO: Add your drawing code here
 
