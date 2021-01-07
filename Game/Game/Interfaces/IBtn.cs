2310c008c72d03c170cc965f332bfc2385e051e0
Yoram Swiers <yoram@swiers.be>
2021-01-07 22:02:48 +0100

    added button interface!

---
+++ Game/Game/Interfaces/IBtn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace IronManGame.Interfaces
+{
+    interface IBtn
+    {
+        void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+        void update(GameTime gameTime);
+
+
+
+    }
+}
